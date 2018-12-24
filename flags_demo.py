73be170c961c5ce78a71309169c04c5ec5c90c45
Andiry Xu <andiryxu@google.com>
2018-12-24 13:08:47 -0800

    flags_demo

---
+++ flags_demo.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+flags = tf.app.flags
+flags.DEFINE_string("data_dir", "/tmp/mnist-data",
+        "Directory for storing mnist data")
+
+FLAGS = flags.FLAGS
+
+def main(_):
+    print(FLAGS.data_dir)
+    pass
+
+if __name__ == "__main__":
+    tf.app.run()
