847bd7cae596103e89b5622895ce151152809655
Andiry Xu <andiryxu@google.com>
2018-12-31 11:34:26 -0800

    Simple path fix

--- simple.py
+++ simple.py
@@ -10,13 +10,20 @@ init = tf.global_variables_initializer()
 options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 run_metadata = tf.RunMetadata()
 
+name = 'simple'
+dump_dir = '/tmp/'
+graph_path = ''.join([name, '.pbtxt'])
+step_stats_path = ''.join([dump_dir, name, '-stepstats.pbtxt'])
+tensorboard_path = ''.join([dump_dir, name])
+meta_graph_path =  ''.join([dump_dir, name, '.meta'])
+
 with tf.Session() as sess:
     init.run()
     print(sess.run(f, options=options, run_metadata=run_metadata))
-    tf.train.write_graph(sess.graph, '/tmp/', 'simple.pbtxt')
-    open('/tmp/simple-stepstats.pbtxt', 'w').write(str(run_metadata.step_stats))
-    writer = tf.summary.FileWriter('/tmp/simple', sess.graph)
+    tf.train.write_graph(sess.graph, dump_dir, graph_path)
+    open(step_stats_path, 'w').write(str(run_metadata.step_stats))
+    writer = tf.summary.FileWriter(tensorboard_path, sess.graph)
     writer.close()
 
-meta_graph_def = tf.train.export_meta_graph(filename='/tmp/simple.meta')
+meta_graph_def = tf.train.export_meta_graph(filename=meta_graph_path)
 
