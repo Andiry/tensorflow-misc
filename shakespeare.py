a087cf661ab2ec5804b21502514aef1907dee235
Andiry Xu <andiryxu@google.com>
2019-04-12 00:10:23 -0700

    Inference

--- shakespeare.py
+++ shakespeare.py
@@ -129,6 +129,7 @@ checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 
 EPOCHS = 5
 
+# Training
 for epoch in range(EPOCHS):
 	start = time.time()
 	
@@ -150,3 +151,48 @@ for epoch in range(EPOCHS):
 
 	print ('Epoch {} Loss {:.4f}'.format(epoch, loss))
 	print ('Time taken for one epoch {} sec\n'.format(time.time() - start))
+
+# Inference
+model.build(tf.TensorShape([1, None]))
+
+# Evaluation step (generating text using the learned model)
+
+# Number of characters to generate
+num_generate = 1000
+
+# You can change the start string to experiment
+start_string = 'Q'
+
+# Converting our start string to numbers (vectorizing)
+input_eval = [char2idx[s] for s in start_string]
+input_eval = tf.expand_dims(input_eval, 0)
+
+# Empty string to store our results
+text_generated = []
+
+# Low temperatures results in more predictable text.
+# Higher temperatures results in more surprising text.
+# Experiment to find the best setting.
+temperature = 1.0
+
+# Evaluation loop.
+
+# Here batch size == 1
+model.reset_states()
+for i in range(num_generate):
+	predictions = model(input_eval)
+
+	# remove the batch dimension
+	predictions = tf.squeeze(predictions, 0)
+
+	# using a multinomial distribution to predict the word returned by the model
+	predictions = predictions / temperature
+	predicted_id = tf.multinomial(predictions, num_samples=1)[-1,0].numpy()
+
+	# We pass the predicted word as the next input to the model
+	# along with the previous hidden state
+	input_eval = tf.expand_dims([predicted_id], 0)
+
+	text_generated.append(idx2char[predicted_id])
+
+print (start_string + ''.join(text_generated))
