455ec0b4d52592c2057d664eec3934e75307197b
Andiry Xu <andiryxu@google.com>
2019-08-12 15:10:53 -0700

    Add device placement for quantized_conv2d

--- quantized_conv2d.py
+++ quantized_conv2d.py
@@ -1,19 +1,17 @@
 import tensorflow as tf
 
-a = tf.constant([1.5, 1.2], shape = [1,1,1,2], name = 'a')
-a1 = tf.constant([[2.0], [1.0]], shape = [1,1,2,1], name = 'b')
+with tf.device('/gpu:0'):
+  a = tf.constant([1.5, 1.2], shape = [1,1,1,2], name = 'a')
+  a1 = tf.constant([[2.0], [1.0]], shape = [1,1,2,1], name = 'b')
 
-min_a = tf.reduce_min(a)
-max_a = tf.reduce_max(a)
+  min_a = tf.reduce_min(a)
+  max_a = tf.reduce_max(a)
+  min_a1 = tf.reduce_min(a1)
+  max_a1 = tf.reduce_max(a1)
 
-x = tf.quantization.quantize(a, min_a, max_a, tf.quint8)
-
-min_a1 = tf.reduce_min(a1)
-max_a1 = tf.reduce_max(a1)
-
-x1 = tf.quantization.quantize(a1, min_a1, max_a1, tf.quint8)
-
-y1 = tf.nn.quantized_conv2d(x.output,x1.output,x.output_min,x.output_max,x1.output_min,x1.output_max,[1,1,1,1],"SAME")
+  x = tf.quantization.quantize(a, min_a, max_a, tf.quint8)
+  x1 = tf.quantization.quantize(a1, min_a1, max_a1, tf.quint8)
+  y1 = tf.nn.quantized_conv2d(x.output,x1.output,x.output_min,x.output_max,x1.output_min,x1.output_max,[1,1,1,1],"SAME")
 
 options = tf.RunOptions()
 options.output_partition_graphs = True
@@ -27,8 +25,8 @@ step_stats_path = ''.join([dump_dir, name, '-stepstats.pbtxt'])
 tensorboard_path = ''.join([dump_dir, name])
 meta_graph_path =  ''.join([dump_dir, name, '.meta'])
 
-with tf.device('/gpu:0'):
-  with tf.Session() as sess:
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                      log_device_placement=True)) as sess:
     print(sess.run(y1, options=options, run_metadata=run_metadata))
     tf.train.write_graph(sess.graph, dump_dir, graph_path)
     open(step_stats_path, 'w').write(str(run_metadata.step_stats))
